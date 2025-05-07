3c79a74ceb123223734a8166dd5c7aa8e740306f
Marc-AntoineJobin <2356735@csfoy.ca>
2025-05-07 08:58:13 -0400

    ajout table actor_film

---
+++ database/migrations/2025_05_07_124144_create_films_critics_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('films_critics', function (Blueprint $table) {
+            $table->primary(['critic_id', 'film_id']);
+            $table->foreignId('critic_id')->constrained();
+            $table->foreignId('film_id')->constrained();
+            $table->integer('review')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('films_critics');
+    }
+};
